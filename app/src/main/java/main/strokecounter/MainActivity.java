532a66472c22c27f83a947cc0ab975fb613dd980
Lasstuu <lassituo@gmail.com>
2024-09-26 16:13:17 +0300

    Initial Commit

---
+++ app/src/main/java/main/strokecounter/MainActivity.java
@@ -0,0 +1,25 @@
+package main.strokecounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void switchToAddCourse(View view){
+        Intent intent = new Intent(this, TabActivity.class);
+        startActivity(intent);
+    }
+
+    public void switchToCourseSelection(View view){
+        Intent intent = new Intent(this, CourseSelectionActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/main/strokecounter/TabPagerAdapter.java
@@ -0,0 +1,33 @@
+package main.strokecounter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import main.strokecounter.fragments.CreateCourseFragment;
+import main.strokecounter.fragments.EditCourseFragment;
+
+public class TabPagerAdapter extends FragmentStateAdapter {
+    public TabPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new CreateCourseFragment();
+            case 1:
+                return new EditCourseFragment();
+            default:
+                return new CreateCourseFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
